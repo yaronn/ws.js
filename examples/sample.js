8a0b3816e4bd568132e0d46dcb4547d720057009
Yaron Naveh <yaronn01@gmail.com>
2012-02-24 22:20:26 +0200

    spaces

--- examples/sample.js
+++ examples/sample.js
@@ -1,22 +1,22 @@
 var ws = require('./lib/ws.js')
 
-  , ctx = 	{request: 	"<Envelope xmlns='http://schemas.xmlsoap.org/soap/envelope/'>" +
-							"<Header />" +
-								"<Body>" +
-									"<EchoString xmlns='http://tempuri.org/'>" +
-										"<s>123</s>" +
-									"</EchoString>" +
-								"</Body>" +
-						"</Envelope>"
-			,	url: "http://localhost/service"
-			,	action: "http://tempuri.org/EchoString"
-				contentType: "text/xml" 
-			}
+  , ctx =  { request: 	"<Envelope xmlns='http://schemas.xmlsoap.org/soap/envelope/'>" +
+                          "<Header />" +
+                            "<Body>" +
+                              "<EchoString xmlns='http://tempuri.org/'>" +
+                                "<s>123</s>" +
+                              "</EchoString>" +
+                            "</Body>" +
+                        "</Envelope>"
+           , url: "http://localhost/service"
+           , action: "http://tempuri.org/EchoString"
+           , contentType: "text/xml" 
+           }
 
-var handlers = 	[ new ws.Addr("http://ws-addressing/v8")
-				, new ws.Http()
-				]
+var handlers =  [ new ws.Addr("http://ws-addressing/v8")
+                , new ws.Http()
+                ]
 
 ws.send(handlers, ctx, function(ctx) {  				    			
-	console.log("response: " + ctx.response);
+  console.log("response: " + ctx.response);
 })
--- examples/wcf-mtom.js
+++ examples/wcf-mtom.js
@@ -4,20 +4,20 @@ var	CustomBinding = require('../lib/proxies/wcf.js').CustomBinding
   ,	Proxy = require('../lib/proxies/wcf.js').Proxy
   ,	fs = require('fs')
   , message = 
-	'<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope">' +
-		'<s:Header />' +
-			'<s:Body>' +
-				'<EchoFiles xmlns="http://tempuri.org/">' +
-					'<value xmlns:a="http://schemas.datacontract.org/2004/07/" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">' +
-						'<a:File1 />' +
-						'<a:File2 />' +
-					'</value>' +
-				'</EchoFiles>' +
-			'</s:Body>' +
-	'</s:Envelope>'
+      '<s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope">' +
+        '<s:Header />' +
+          '<s:Body>' +
+            '<EchoFiles xmlns="http://tempuri.org/">' +
+              '<value xmlns:a="http://schemas.datacontract.org/2004/07/" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">' +
+                '<a:File1 />' +
+                '<a:File2 />' +
+              '</value>' +
+            '</EchoFiles>' +
+          '</s:Body>' +
+      '</s:Envelope>'
   , binding = new CustomBinding([new MtomMessageEncodingBindingElement({MessageVersion: "Soap12WSAddressing10"}),
-								,new HttpTransportBindingElement()
-								])
+                                ,new HttpTransportBindingElement()
+                                ])
   , proxy = new Proxy(binding, "http://localhost:7171/Service/mtom")
 
 proxy.addAttachment("//*[local-name(.)='File1']", "./test/unit/client/files/p.jpg");
